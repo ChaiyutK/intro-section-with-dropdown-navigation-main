515783aff146e5903f8fddf5f3088772160cdb12
Chaiyut <benzcyber53@gmail.com>
2022-07-26 17:27:23 +0700

    git init

---
+++ src/components/Attribution.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Attribution = () => {
+  return (
+    <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>. 
+    Coded by <a href="https://www.linkedin.com/in/chaiyut-ck/" rel="noopener noreferrer">Your Name Here</a>.
+  </div>
+  )
+}
+
+export default Attribution
