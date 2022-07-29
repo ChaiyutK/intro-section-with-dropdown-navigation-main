21031de5dd712fe13a9082ff097944d0d4dfec98
Chaiyut <benzchaiyut.k@gmail.com>
2022-07-29 18:08:32 +0700

    add sizehandle function

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Attribution = () => {
   return (
-    <div class="attribution">
+    <div className="attribution">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>. 
     Coded by <a href="https://www.linkedin.com/in/chaiyut-ck/" rel="noopener noreferrer">Your Name Here</a>.
   </div>
